c132ce19cd86e357f2cff7161e24cffc9b58bcc7
Zinvert <83873378+BKS8@users.noreply.github.com>
2022-12-31 10:28:12 +0530

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("score.txt") as demo:
+            self.high_score = int(demo.read())
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(
+            f"Score: {self.score} Hign Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("score.txt", mode="w") as demo:
+                demo.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
